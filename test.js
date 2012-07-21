bfe8dec1c3f58d6a6edf5e893eed6aa3b6f6325d
Carmen Wick <CarmenWick@gmail.com>
2012-07-20 20:27:37 -0700

    Use absolute positioning and block style

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 // Create a new YUI instance and populate it with the required modules.
 YUI().use('overlay', function (Y) {
-  var Test = Y.Base.create("Test", Y.Overlay, [], {
+  var SpriteEditor = Y.Base.create("SpriteEditor", Y.Overlay, [], {
     renderUI: function() {
-      for (var row=0; row<Test.GRID_ROWS; row++) {
-        for (var column=0; column<Test.GRID_COLUMNS; column++) {
+      for (var row=0; row<SpriteEditor.GRID_ROWS; row++) {
+        for (var column=0; column<SpriteEditor.GRID_COLUMNS; column++) {
           console.log(row, column);
           this._createGridCell(row, column);
         }
@@ -11,43 +11,40 @@ YUI().use('overlay', function (Y) {
     },
 
     _createGridCell: function(row, column) {
+      var width = (this.get("width") / SpriteEditor.GRID_COLUMNS) - 2;
+      var height = (this.get("height") / SpriteEditor.GRID_ROWS) - 2;
       var cell = Y.Node.create('<div></div>'),
           contentBox = this.get("contentBox");
 
       cell.addClass(this.getClassName('grid-cell'));
 
-      if (row == Test.GRID_ROWS-1) {
+      if (row == SpriteEditor.GRID_ROWS-1) {
         cell.addClass(this.getClassName('grid-cell-bottom'));
       }
-      if (column == Test.GRID_COLUMNS-1) {
+      if (column == SpriteEditor.GRID_COLUMNS-1) {
         cell.addClass(this.getClassName('grid-cell-right'));
       }
 
       cell.setStyles({
-        width: (this.get("width") / Test.GRID_COLUMNS) - 2,
-        height: (this.get("height") / Test.GRID_ROWS) - 2
+        width: (this.get("width") / SpriteEditor.GRID_COLUMNS) - 2,
+        height: (this.get("height") / SpriteEditor.GRID_ROWS) - 2,
+        left: column*width,
+        top: row*height
       });
 
       contentBox.append(cell);
     }
+  }, {
+    GRID_ROWS: 10,
+    GRID_COLUMNS: 10,
+    ATTRS: {
+      width: { value: 500 },
+      height: { value: 500 },
+      centered: { value: true }
+    }
   });
-  Test.GRID_ROWS=10;
-  Test.GRID_COLUMNS=10;
 
-  var overlay = new Test({
-    width: 500,
-    height: 500,
-    centered: true
-  });
-  overlay.render();
+  var editor = new SpriteEditor();
+  editor.render();
 });
 
-
-// middle
-// top
-// bottom
-// left
-// top-left
-// top-right
-// bottom-left
-// bottom-right
