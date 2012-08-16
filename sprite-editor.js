826827808e0a390c4d80a036f44a84ce0cb81b73
Carmen Wick <CarmenWick@gmail.com>
2012-08-15 20:45:48 -0700

    Add click-and-drag painting

--- sprite-editor.js
+++ sprite-editor.js
@@ -1,8 +1,11 @@
 YUI.add('game-sprite-editor', function(Y) {
 var SpriteEditor = Y.Base.create("SpriteEditor", Y.Overlay, [], {
   initializer: function() {
+    this._initClasses();
     this._initStylesheet();
     this._initImage();
+
+    this._isPainting = false;
   },
 
   renderUI: function() {
@@ -15,12 +18,30 @@ var SpriteEditor = Y.Base.create("SpriteEditor", Y.Overlay, [], {
 
   bindUI: function() {
     var contentBox = this.get("contentBox");
-    contentBox.on("mouseover", function(e) {
-      this._activateGridCell(e.target);
-    }, this);
+    contentBox.on("gesturemoveend", function(e) {
+      this._isPainting = false;
+    }, null, this);
+
+    contentBox.on("gesturemove", function(e) {
+      if (this._isPainting &&
+          e.target.hasClass(this.CSS_GRID_CELL)) {
+        this._activateGridCell(e.target);
+      }
+    }, null, this);
+
+    contentBox.on("gesturemovestart", function(e) {
+      this._isPainting = true;
+    }, null, this);
+
     contentBox.on("click", function(e) {
       this._activateGridCell(e.target);
     }, this);
+  },
+
+  _initClasses: function() {
+    this.CSS_GRID_CELL = this.getClassName("grid-cell");
+    this.CSS_GRID_CELL_BOTOM = this.getClassName("grid-cell-bottom");
+    this.CSS_GRID_CELL_RIGHT = this.getClassName("grid-cell-right");
   },
 
   _initStylesheet: function() {
@@ -28,7 +49,7 @@ var SpriteEditor = Y.Base.create("SpriteEditor", Y.Overlay, [], {
     var cellHeight = this.get("height") / SpriteEditor.GRID_ROWS;
     var stylesheet = Y.one("#test_stylesheet").getDOMNode().sheet;
 
-    var gridDimensions = "." + this.getClassName("grid-cell") +
+    var gridDimensions = "." + this.CSS_GRID_CELL +
       "{ width: " + cellWidth + ";" +
       "  height: " + cellHeight + ";" +
       "}";
@@ -61,15 +82,15 @@ var SpriteEditor = Y.Base.create("SpriteEditor", Y.Overlay, [], {
     var cell = Y.Node.create('<div></div>'),
         contentBox = this.get("contentBox");
 
-    cell.addClass(this.getClassName('grid-cell'));
+    cell.addClass(this.CSS_GRID_CELL);
     cell.setData('row', row);
     cell.setData('column', column);
 
     if (row == SpriteEditor.GRID_ROWS-1) {
-      cell.addClass(this.getClassName('grid-cell-bottom'));
+      cell.addClass(this.CSS_GRID_CELL_BOTOM);
     }
     if (column == SpriteEditor.GRID_COLUMNS-1) {
-      cell.addClass(this.getClassName('grid-cell-right'));
+      cell.addClass(this.CSS_GRID_CELL_RIGHT);
     }
 
     cell.setStyles({
@@ -92,5 +113,5 @@ var SpriteEditor = Y.Base.create("SpriteEditor", Y.Overlay, [], {
 });
 
 Y.namespace("Game").SpriteEditor = SpriteEditor;
-}, "", { requires: ['overlay', 'game-image'] });
+}, "", { requires: ['event-move', 'overlay', 'game-image'] });
 
